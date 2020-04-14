652c9ec8f8302fe61e101ce9df22019fb64557a7
davidshiraku <davidshiraku@yahoo.com>
2020-04-14 06:22:27 +0300

    Debug 14/Apr/2020 0622

--- src/estimator.php
+++ src/estimator.php
@@ -29,7 +29,7 @@ function covid19ImpactEstimator($data)
   //End of normalizing days depending on period type
   
   //Determination of interval of 3 days
-  $interval = floor($timeelapse / 3);
+  $interval = sprintf('%.0f', $timeelapse / 3);
   //End of determination of interval of 3 days
   
   /*$currentlyInfectedrequestedtime = $currentlyInfected * (pow(2,$interval));
@@ -61,20 +61,20 @@ function covid19ImpactEstimator($data)
   $currentlyInfecteddollarsInFlight = $currentlyInfectedrequestedtime * $result["region"]["avgDailyIncomePopulation"] * $result["region"]["avgDailyIncomeInUSD"] * $timeelapse;
   $severeImpactdollarsInFlight = $severeImpactrequestedtime * $result["region"]["avgDailyIncomePopulation"] * $result["region"]["avgDailyIncomeInUSD"] * $timeelapse;
   
-  $currentlyInfected = floor($currentlyInfected);
-  $severeImpact = floor($severeImpact);
-  $currentlyInfectedrequestedtime = floor($currentlyInfectedrequestedtime);
-  $severeImpactrequestedtime = floor($severeImpactrequestedtime);
-  $currentlyInfectedrequestedtimesevere = floor($currentlyInfectedrequestedtimesevere);
-  $severeImpactrequestedtimesevere = floor($severeImpactrequestedtimesevere);
-  $currentlyInfectedhospitalBedsByRequestedTime = floor($currentlyInfectedhospitalBedsByRequestedTime);
-  $severeImpacthospitalBedsByRequestedTime = floor($severeImpacthospitalBedsByRequestedTime);
-  $currentlyInfectedcasesForICUByRequestedTime = floor($currentlyInfectedcasesForICUByRequestedTime);
-  $severeImpactcasesForICUByRequestedTime = floor($severeImpactcasesForICUByRequestedTime);
-  $currentlyInfectedcasesForVentilatorsByRequestedTime = floor($currentlyInfectedcasesForVentilatorsByRequestedTime);
-  $severeImpactcasesForVentilatorsByRequestedTime = floor($severeImpactcasesForVentilatorsByRequestedTime);
-  $currentlyInfecteddollarsInFlight = floor($currentlyInfecteddollarsInFlight);
-  $severeImpactdollarsInFlight = floor($severeImpactdollarsInFlight);
+  $currentlyInfected = sprintf('%.0f', $currentlyInfected);
+  $severeImpact = sprintf('%.0f', $severeImpact);
+  $currentlyInfectedrequestedtime = sprintf('%.0f', $currentlyInfectedrequestedtime);
+  $severeImpactrequestedtime = sprintf('%.0f', $severeImpactrequestedtime);
+  $currentlyInfectedrequestedtimesevere = sprintf('%.0f', $currentlyInfectedrequestedtimesevere);
+  $severeImpactrequestedtimesevere = sprintf('%.0f', $severeImpactrequestedtimesevere);
+  $currentlyInfectedhospitalBedsByRequestedTime = sprintf('%.0f', $currentlyInfectedhospitalBedsByRequestedTime);
+  $severeImpacthospitalBedsByRequestedTime = sprintf('%.0f', $severeImpacthospitalBedsByRequestedTime);
+  $currentlyInfectedcasesForICUByRequestedTime = sprintf('%.0f', $currentlyInfectedcasesForICUByRequestedTime);
+  $severeImpactcasesForICUByRequestedTime = sprintf('%.0f', $severeImpactcasesForICUByRequestedTime);
+  $currentlyInfectedcasesForVentilatorsByRequestedTime = sprintf('%.0f', $currentlyInfectedcasesForVentilatorsByRequestedTime);
+  $severeImpactcasesForVentilatorsByRequestedTime = sprintf('%.0f', $severeImpactcasesForVentilatorsByRequestedTime);
+  $currentlyInfecteddollarsInFlight = sprintf('%.0f', $currentlyInfecteddollarsInFlight);
+  $severeImpactdollarsInFlight = sprintf('%.0f', $severeImpactdollarsInFlight);
   
   $data = array();
   
